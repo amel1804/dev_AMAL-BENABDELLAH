1b9b8fc2b59bacbc8cc9c3942cbb4b8d2c2e5ce4
Amal1804 <abenabdellah@helb-prigogine.be>
2022-12-21 14:47:09 +0100

    Update sale_order_line.py

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -2,5 +2,4 @@ from odoo import fields, models, Command
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-    training_date = fields.Char(string="Training Date")
-    
+    training_date = fields.Date(string="Training Date")
