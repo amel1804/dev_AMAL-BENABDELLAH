bd74e9d1774b6971f25bbbe258562239e5d8d6ca
Amal1804 <abenabdellah@helb-prigogine.be>
2022-12-21 15:57:38 +0100

    Update sale_order_line.py

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,4 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
-    res_partner = fields.Many2one(string="Employee")
+    employee_id = fields.Many2one('hr.employee', string="Employee", required=True, ondelete='cascade', index=True)
