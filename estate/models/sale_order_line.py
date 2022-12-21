a046a29d4df31738da54c2125849f07302f3ab0d
Amal1804 <abenabdellah@helb-prigogine.be>
2022-12-21 15:35:13 +0100

    push

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -3,3 +3,4 @@ from odoo import fields, models, Command
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
     training_date = fields.Date(string="Training Date")
+    res_partner = fields.Many2one(string="Employee")
