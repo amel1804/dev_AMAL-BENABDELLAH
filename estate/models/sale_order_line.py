a5321ebf2a71a6542918ec4bd9a203655e6675f6
Amal1804 <abenabdellah@helb-prigogine.be>
2023-01-08 22:19:09 +0100

    Update sale_order_line.py

--- estate/models/sale_order_line.py
+++ estate/models/sale_order_line.py
@@ -5,15 +5,15 @@ class SaleOrderLine(models.Model):
     training_date = fields.Date(string="Training Date")
     employee_id = fields.Many2one('hr.employee', string="Employee")
 
-    @api.onchange('employee_id', 'training_date')
-    def create_event(self):
-        calendar_event_obj = self.env['calendar.event']
-        for record in self:
-            if record.employee_id and record.training_date:
-                event_name = 'Formation pour employé ' + record.employee_id.name
-                calendar_event_obj.create({
-                    'name': event_name,
-                    'start_datetime': record.training_date,
-                    'stop_datetime': record.training_date,
-                    'partner_ids': [(4, record.employee_id.user_id.partner_id.id)],
-                })
+    # @api.onchange('employee_id', 'training_date')
+    # def create_event(self):
+    #     calendar_event_obj = self.env['calendar.event']
+    #     for record in self:
+    #         if record.employee_id and record.training_date:
+    #             event_name = 'Formation for employee ' + record.employee_id.name
+    #             calendar_event_obj.create({
+    #                 'name': event_name,
+    #                 'start_datetime': record.training_date,
+    #                 'stop_datetime': record.training_date,
+    #                 'partner_ids': [(4, record.employee_id.user_id.partner_id.id)],
+    #             })
